33139292b85f451b45ce227faa58a14dfe7a7059
Ben Aw Yong <herbalvegetable@gmail.com>
2023-04-16 01:12:37 +0800

    update leftsidebar link hover css

--- webapp/src/components/LeftSidebar/LeftSidebar.js
+++ webapp/src/components/LeftSidebar/LeftSidebar.js
@@ -1,9 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, use } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { BsHexagonHalf, BsFillHouseFill, BsFillSendFill } from 'react-icons/bs';
 
 import styles from './LeftSidebar.module.css';
+
+function LinkContainer(props) {
+    const [isHover, setIsHover] = useState(false);
+
+    const { href, title, Icon, router } = props;
+
+    const [isActive, setIsActive] = useState(false);
+    useEffect(() => {
+        setIsActive(router.pathname == href);
+    }, [router]);
+
+    return (
+        <div
+            className={styles.link_container}
+            onClick={e => {
+                router.push(href);
+            }}
+            onMouseEnter={e => {
+                setIsHover(true);
+            }}
+            onMouseLeave={e => {
+                setIsHover(false);
+            }}>
+            <div className={`${styles.active_container} ${isActive ? styles.active : ''} ${isHover ? styles.hover : ''}`}>
+                <Icon
+                    className={styles.icon}
+                    color={isActive ? 'white' : 'black'} />
+                {title}
+            </div>
+        </div>
+    )
+}
 
 export default function LeftSidebar(props) {
     const router = useRouter();
@@ -36,19 +68,10 @@ export default function LeftSidebar(props) {
 
                             const isActive = router.pathname == href;
                             return (
-                                <div
+                                <LinkContainer
                                     key={i.toString()}
-                                    className={styles.link_container}
-                                    onClick={e => {
-                                        router.push(href);
-                                    }}>
-                                    <div className={`${styles.active_container} ${isActive ? styles.active : ''}`}>
-                                        <Icon
-                                            className={styles.icon}
-                                            color={isActive ? 'white' : 'black'} />
-                                        {title}
-                                    </div>
-                                </div>
+                                    {...l}
+                                    router={router} />
                             )
                         })
                     }
